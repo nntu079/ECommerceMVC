dcb5b84bf2307556cb6e798d75aeac1e4f0371c5
Win 10 <nntu079@gmail.com>
2024-01-26 10:13:36 +0700

    Add project files.

---
+++ ECommerceMVC/Controllers/HangHoaController.cs
@@ -0,0 +1,37 @@
+﻿using ECommerceMVC.Data;
+using ECommerceMVC.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceMVC.Controllers
+{
+    public class HangHoaController : Controller
+    {
+        private readonly Hshop2023Context db;
+        public HangHoaController(Hshop2023Context context)
+        {
+
+            db = context;
+        }
+        public IActionResult Index(int? loai)
+        {
+            var hangHoas = db.HangHoas.AsQueryable();
+
+            if (loai.HasValue)
+            {
+                hangHoas.Where( p => p.MaLoai == loai.Value );
+            }
+
+            var result = hangHoas.Select(p => new HangHoaVM
+            {
+                MaHh = p.MaHh,
+                TenHH = p.TenHh,
+                DonGia = p.DonGia ?? 0,
+                Hinh = p.Hinh ?? "",
+                MoTaNgan = p.MoTaDonVi ?? "",
+                TenLoai = p.MaLoaiNavigation.TenLoai
+            });
+
+            return View(result);
+        }
+    }
+}
